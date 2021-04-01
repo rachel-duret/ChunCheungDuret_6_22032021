4894706bd0c8c9ac996f4a02ecc3c5239ecabd13
rachelduret <chuncheung.ku@gmail.com>
2021-04-01 15:21:02 +0200

    add morgan for logging

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-//const cookieParser = require('cookie-parser');
-//const session= require('express-session');
 const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
 const fs = require('fs');
 
-/* helmet :
-* X-Powered-BY:cacher la langue  
-*X-XSS- Protection*/
+
+/* helmet :1* X-Powered-BY  2* X-XSS- Protection; 3* X-permittedCrossDomainPolicies; 4*frameguard; 5*ieNoOpen; 6* dnsPrefetchControl; 7* originAgentCluster; 8*noSniff; 9*hsts; 10* referrerPolicy; 11*expectCt; 12*contentSecurityPolicy */
 app.use(helmet());
+// surviller logging----OWASP(10: Insufficient logging & Monitoring)
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 //app.use(cookieParser('name', 'httpOnly'));
 
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Content-Security-Policy', "default-src 'self'");//against Xss attack
+   // res.setHeader('Content-Security-Policy', "default-src 'self'");//against Xss attack
     next();
   });
 
