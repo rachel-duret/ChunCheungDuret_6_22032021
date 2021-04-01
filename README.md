39ae51a9e245f73237e2b1b1730bb5bbcb8c5e28
rachelduret <chuncheung.ku@gmail.com>
2021-04-01 11:35:16 +0200

    add reg expreion for login

---
+++ README.md
@@ -0,0 +1,14 @@
+Open classroom developper web projet 6 PIQUANTE---
+## lancer le projet 
+## frontend git clone :git clone git@github.com:OpenClassrooms-Student-Center/dwj-projet6.git front 
+# Piquante
+
+Le projet a été généré avec [Angular CLI](https://github.com/angular/angular-cli) version 7.0.2.
+
+Pour faire fonctionner le projet, vous devez installer node-sass à part.
+
+## Development server
+
+Démarrer `ng serve` pour avoir accès au serveur de développement. Rendez-vous sur `http://localhost:4200/`. L'application va se recharger automatiquement si vous modifiez un fichier source.
+## backend: npm install 
+## npm server.js   ou install nodemon : nodemon server
--- controllers/user.js
+++ controllers/user.js
@@ -4,17 +4,30 @@ const jwt = require('jsonwebtoken');
 
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ message:'conect'+error }));
+  const email = req.body.email;
+  const password = req.body.password;
+  //pour verifier un mot de pass au mions Au moins 8 à 16 caractères, au moins 1 lettre majuscule, 1 lettre minuscule et 1 chiffre, les autres peuvent être n'importe quel caractère:
+  const regPasswordCheck =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/g;
+  //pour verifier le format d'email  est correct
+  const regEmailCheck = /\b[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,6}\b/g;
+  if (!email.match(regEmailCheck)){
+    throw new Error("Votre email n'est pas crroect !")
+  }else if(!password.match(regPasswordCheck)){
+    throw new Error("Votre password n'est pas assez sécurisé !")
+  }else{
+    bcrypt.hash(password, 10)
+    .then(hash => {
+      const user = new User({
+        email: email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ message:'conect'+error }));
+  }
+    
   };
 
   exports.login = (req, res, next) => {
